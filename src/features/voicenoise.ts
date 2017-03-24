5601f3333185d65cce22cc3084bc4d1b4d4d0b49
Connor Hilarides <connorcpu@live.com>
2017-03-24 16:12:04 -0700

    tiny violins (#4)

--- src/features/voicenoise.ts
+++ src/features/voicenoise.ts
@@ -74,6 +74,11 @@ const NOISES: Noise[] = [
         "id": "HUEYS",
         "file": _pathForNoiseFile("hueys.mp3"),
         "regex": [/^(distant[\s]+)?(hu(ey|ie)s|chopper(s?))$/i]
+    },
+    {
+        "id": "SADVIOLIN",
+        "file": _pathForNoiseFile("hueys.mp3"),
+        "regex": [/^(sad|tiny)[\s]+violins?$/i]
     }
 ]
 
